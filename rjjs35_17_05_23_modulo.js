6ea8da5863e029560a86e6bda67d268eeef2741f
shubham7354 <shubhamkanaujiya.apn@gmail.com>
2023-05-17 18:18:16 +0530

    module

---
+++ rjjs35_17_05_23_modulo.js
@@ -0,0 +1,14 @@
+// const name="shubham";
+// const age=22;
+
+// export {name,age}
+
+
+const name="shubham Kanaujiya";
+const school="sst";
+
+const sk=()=>{
+    return "welcome shubham";
+}
+
+export default sk;
